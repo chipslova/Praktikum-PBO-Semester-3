f1850449ac810a402b43e8803788bbad39dbe4bc
chipslova <114897562+chipslova@users.noreply.github.com>
2023-10-25 22:23:17 +0700

    Add files via upload

---
+++ T3_Luthfi Sifa Khaerunnisa_Praktikum PBO/src/mtk_turunan/Matematika2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mtk_turunan;
+
+import mtkawal.Matematika;
+
+/**
+ *
+ * @author luthf
+ */
+public class Matematika2 extends Matematika{
+    
+    public int modulus (int a, int b){
+        int hasil = a % b;
+        return hasil;
+    }
+}
